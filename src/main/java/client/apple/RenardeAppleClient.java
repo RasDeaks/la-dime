33d79b38a04e525589281b8dce271e61b83f70ee
dix <francois.dixmeras@lunatech.fr>
2024-03-07 16:08:38 +0100

    Fix Apple revoke use case : rm forgotten annotation

--- src/main/java/client/apple/RenardeAppleClient.java
+++ src/main/java/client/apple/RenardeAppleClient.java
@@ -2,7 +2,6 @@ package client.apple;
 
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(baseUri = "https://appleid.apple.com")
@@ -11,10 +10,9 @@ public interface RenardeAppleClient {
     @POST
     @Path("/auth/revoke")
     @Produces(MediaType.APPLICATION_FORM_URLENCODED)
-    void revokeAppleUser(@FormParam("client_id") @ConfigProperty(name = "quarkus.oidc.apple.client-id") String clientID,
+    void revokeAppleUser(@FormParam("client_id") String clientID,
                              @FormParam("client_secret") String clientSecret,
                              @FormParam("token") String token,
-                             @FormParam("token_type_hint") String tokenTypeHint
-                                );
+                             @FormParam("token_type_hint") String tokenTypeHint);
 
 }
